d8baedac6e6f9540061379e0a61995ae34568a0d
aliparsafar <aliparsafar.code@gmail.com>
2025-03-11 11:32:53 +0330

    edit serachmovies component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
-
-import './App.css'
+import { Routes, Route } from "react-router-dom";
+import SearchMovies from "./components/searchmovies";
 
 function App() {
- 
-
   return (
-    <div>
-      
+    <div className="container mx-auto flex flex-col justify-center items-center mt-50 ">
+      <h1 className="text-4xl font-bold text-center my-4">Movie Search</h1>
+      <p className="text-2xl">Search for your favorite movies and rate them</p>
+      <Routes>
+        <Route path="/" element={<SearchMovies />} />
+      </Routes>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/searchmovies.tsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_OMDB_API_KEY;
+
+const fetchMovies = async (searchTerm: string) => {
+  if (!searchTerm) return [];
+  const { data } = await axios.get(
+    `https://www.omdbapi.com/?s=${searchTerm}&apikey=${API_KEY}`
+  );
+  return data.Search || [];
+};
+
+
+
+  return (
+    <div>
+
+    </div>
+  );
+};
+
+export default SearchMovies;
